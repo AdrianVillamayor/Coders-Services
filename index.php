c2731ca88ff4e2a29aadcadbecb1a125c43c9eaa
Adrian <adrian.villamayor@outlook.com>
2020-03-03 18:39:39 +0100

    COre

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="keywords" content="">
 
-    <title>Code</title>
-
+    <title>Coders Services</title>
 
     <link rel="apple-touch-icon" sizes="180x180" href="img/fav/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="img/fav/favicon-32x32.png">
@@ -25,12 +24,16 @@
     <link rel="manifest" href="img/fav/site.webmanifest">
     <link rel="mask-icon" href="img/fav/safari-pinned-tab.svg" color="#ec0256">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ec0256">">
+    <meta name="theme-color" content="#ec0256">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css?<?=time()?>">
 </head>
 
 <body>
 
-
+    <?=phpinfo()?>
 
 </body>
 
