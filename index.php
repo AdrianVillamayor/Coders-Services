38488f778b8b58fd0fb0541bd2f42832f3c78c86
Adrian <adrian.villamayor@outlook.com>
2020-03-04 11:15:03 +0100

    asd

--- index.php
+++ index.php
@@ -45,7 +45,8 @@
   <meta name="twitter:image" content="img/share-webslides.jpg"> <!-- EDIT -->
 
   <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <link rel="stylesheet" href="css/main.css?<?=time()?>">
 
@@ -55,10 +56,10 @@
 
   <main role="main">
 
-  <div class="fixed-top p-4"> <img class="img-fluid w-5" src="img/logo.png" ></div>
+    <div class="fixed-top p-4"> <img class="img-fluid w-5" src="img/logo.png"></div>
 
     <article id="webslides" class="vertical">
-      
+
       <section>
         <div class="wrap aligncenter">
           <h1><strong>Multi-platform application development </strong></h1>
@@ -76,12 +77,12 @@
           </p>
         </div>
       </section>
-     
+
       <section>
 
         <div class="wrap">
-          <h2><strong > Why Coders Services ? </strong></h2>
-         
+          <h2><strong> Why Coders Services ? </strong></h2>
+
           <ul class="flexblock gallery m">
             <li>
               <a href="demos/landings.html" title="Landings">
@@ -106,7 +107,8 @@
             <li>
               <a href="demos/keynote.html" title="Apple Keynote">
                 <figure>
-                  <img alt="Native & Hybrid ANDROID & iOS apps" style="padding: 65px;" class="img-fluid" src="img/app.svg">
+                  <img alt="Native & Hybrid ANDROID & iOS apps" style="padding: 65px;" class="img-fluid"
+                    src="img/app.svg">
                   <figcaption>
                     <h2>Native & Hybrid ANDROID & iOS apps </h2>
                   </figcaption>
@@ -116,46 +118,88 @@
           </ul>
         </div>
       </section>
-      
-      <section>
+
+      <section id="section-4" class="slide current" style="">
         <div class="wrap">
-          <div class="grid vertical-align">
-            <div class="column">
-              <h4>
-                <svg class="fa-life-ring">
-                  <use xlink:href="#fa-life-ring"></use>
-                </svg>
-                <strong>Guides</strong>
-              </h4>
-              <p>If you need help, here's just three tutorials. Just a basic knowledge of HTML is required:</p>
-              <ul class="description">
-                <li><a href="demos/components.html" title="WebSlides Components">WebSlides Components</a>.</li>
-                <li><a href="demos/classes.html" title="WebSlides Classes">WebSlides Classes</a>.</li>
-                <li><a href="demos/media.html" title="WebSlides Media">WebSlides Media: images, videos...</a></li>
-              </ul>
-            </div>
-            <div class="column">
-              <figure><img class="aligncenter" src="img/setup.png" alt="WebSlides Files"></figure>
-            </div>
-            <div class="column">
-              <h4>
-                <svg class="fa-cubes">
-                  <use xlink:href="#fa-cubes"></use>
-                </svg>
-                <strong>Built to expand</strong>
-              </h4>
-              <p>The best way to <strong>inspire with your content</strong> is to connect on a personal level:</p>
-              <ul class="description">
-                <li>Background images: <a href="http://unsplash.com">Unsplash</a>.</li>
-                <li>CSS animations: <a href="https://daneden.github.io/animate.css/">Animate.css</a>.</li>
-                <li>Longforms: <a href="http://michalsnik.github.io/aos/"> Animate on scroll</a>.</li>
-              </ul>
-            </div>
-          </div>
-          <!--end .grid -->
+          <h2>WEB TECHNOLOGY</h2>
+
+          <ul class="flexblock features">
+            
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  PHP
+                </h2>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  GO
+                </h2>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  PYTHON
+                </h2>
+              </div>
+            </li>
+            
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  JAVASCRIPT
+                </h2>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  JAVA
+                </h2>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  API REST
+                </h2>
+              </div>
+            </li>
+            
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                  MySQL
+                </h2>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>
+                  <span>→</span>
+                   Mongo DB
+                </h2>
+              </div>
+            </li>
+
+            
+          </ul>
         </div>
       </section>
-      
+      </section>
+
       <section class="aligncenter">
         <div class="wrap">
           <h2><strong>Ready to Start?</strong> </h2>
@@ -208,7 +252,8 @@
 </body>
 
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 <script src="js/plugins/webslides.min.js?<?=time()?>"></script>
 <script src="js//main.js?<?=time()?>"></script>
