d70cd5ce5e22c00f12f28528137dd05aa48fb31c
Adrian <adrian.villamayor@outlook.com>
2020-03-05 13:37:11 +0100

    asd

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
-require_once("Core/redirecto.php");
 ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("Core/redirecto.php");
+
 ?>
 <!DOCTYPE html>
 
