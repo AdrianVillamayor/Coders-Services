ce8e09575e952f3a9e41f8588d0e31e7e49fe717
Victor Bautista <victor.bautista@gmail.com>
2020-03-06 09:40:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if ($lang == 'es') {
+if (isset($lang) && $lang == 'es') {
   $web_lang = "es_ES.utf8";
 } else {
   $web_lang = "en_EN.utf8";
