0563773f54c3ec5c2919dcc37107e86e4866dc75
Adrian <adrian.villamayor@outlook.com>
2020-03-06 10:32:20 +0100

    test

--- Core/multilang.php
+++ Core/multilang.php
@@ -1,20 +1,10 @@
 <?php
+
 $url = $_SERVER["REQUEST_URI"];
 
-$spanishLanguage = array('eu','ca','es','an','gl');
-
-$lang = 'en';
-
-if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
-    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    if (in_array($lang, $spanishLanguage)) {
-        $lang = "es";
-    }else{
-        $lang = "en";
-    }
-}
-
 $explode_url = explode('/', $url);
+
+$lang = "en";
 
 if (in_array("en", $explode_url)) {
     $lang = 'en';
@@ -23,13 +13,16 @@ if (in_array("es", $explode_url)) {
     $lang = 'es';
 }
 
-if ($lang == 'es') $locale = 'es_ES';
-// if ($lang == 'ca') $locale = 'ca_ES';
-if ($lang == 'en') $locale = 'en_US';
+if (isset($lang) && $lang == 'en') {
+  $web_lang = "en_EN";
+} else {
+  $web_lang = "es_ES";
+  $lang = "es";
+}
 
-setlocale(LC_ALL, $locale);
+setlocale(LC_ALL, $web_lang.".utf8");
 
-$domain = 'coders.services';
+$domain = 'coders';
 $locale_directory = '/var/www/html/locale/';
 
 bindtextdomain($domain, $locale_directory);
--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-
-$url = $_SERVER["REQUEST_URI"];
-
-$explode_url = explode('/', $url);
-
-if (in_array("en", $explode_url)) {
-    $lang = 'en';
-}
-if (in_array("es", $explode_url)) {
-    $lang = 'es';
-}
-
-if (isset($lang) && $lang == 'en') {
-  $web_lang = "en_EN";
-} else {
-  $web_lang = "es_ES";
-  $lang = "es";
-}
-
-setlocale(LC_ALL, $web_lang.".utf8");
-
-$domain = 'coders';
-$locale_directory = '/var/www/html/locale/';
-
-bindtextdomain($domain, $locale_directory);
-textdomain($domain);
-
+  require_once("Core/multilang.php");
 ?>
 
 <!DOCTYPE html>
